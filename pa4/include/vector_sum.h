7a1ae2f7751c23e9e4979eab017019201bde2243
Luke Fraser <lukefrasera@gmail.com>
2015-02-12 22:15:52 -0800

    adding pa4 files

---
+++ pa4/include/vector_sum.h
@@ -0,0 +1,18 @@
+// "vector_sum.h"
+//
+//  Copyright (c) Luke Fraser 2015
+//
+//  This file is part of cs791vlass.
+//
+//    cs791cClass is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    cs791vClass is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with cs791vClass.  If not, see <http://www.gnu.org/licenses/>.
